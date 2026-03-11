1292238eee3a0da4d99ad651d137cbdef5b0bf0c
Nikolay Panov <pannikolaserg@gmail.com>
2026-03-11 19:23:14 +0900

    using lint group and correcting code

--- src/DB_Manager.py
+++ src/DB_Manager.py
@@ -1,18 +1,21 @@
+from typing import List, Optional, Tuple
+
 import psycopg2
 
 
 class DBManager:
-    def __init__(self, db_name, user, password):
+    def __init__(self, db_name: str, user: str, password: str) -> None:
         self.conn = psycopg2.connect(
             host="localhost",
-            database="postgres",
-            user="postgres",
-            password="Base_2026"
+            database=db_name,
+            user=user,
+            password=password
         )
         self.cursor = self.conn.cursor()
 
-    def create_tables(self):
-        self.cursor.execute("""
+    def create_tables(self) -> None:
+        self.cursor.execute(
+            """
             DROP TABLE IF EXISTS vacancies;
             DROP TABLE IF EXISTS companies;
 
@@ -29,24 +32,41 @@ class DBManager:
                 url VARCHAR(255),
                 company_id INTEGER REFERENCES companies(id)
             );
-        """)
+        """
+        )
         self.conn.commit()
 
-    def insert_company(self, id, name):
+    def insert_company(self, id: int, name: str) -> None:
+        """Добавление компании в БД"""
         self.cursor.execute(
             "INSERT INTO companies (id, name) VALUES (%s, %s)",
             (id, name)
         )
         self.conn.commit()
 
-    def insert_vacancy(self, id, name, salary_from, salary_to, url, company_id):
-        self.cursor.execute("""
+    def insert_vacancy(
+        self,
+        id: int,
+        name: str,
+        salary_from: Optional[int],
+        salary_to: Optional[int],
+        url: str,
+        company_id: int
+    ) -> None:
+        """Добавление вакансии в БД"""
+        self.cursor.execute(
+            """
             INSERT INTO vacancies (id, name, salary_from, salary_to, url, company_id)
             VALUES (%s, %s, %s, %s, %s, %s)
-        """, (id, name, salary_from, salary_to, url, company_id))
+            """,
+            (id, name, salary_from, salary_to, url, company_id),
+        )
         self.conn.commit()
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> List[Tuple[str, int]]:
+        """
+        Получает список всех компаний и количество вакансий у каждой компании
+        """
         self.cursor.execute("""
             SELECT c.name, COUNT(v.id)
             FROM companies c
@@ -55,7 +75,10 @@ class DBManager:
         """)
         return self.cursor.fetchall()
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> List[Tuple[str, str, Optional[int], Optional[int], str]]:
+        """
+        Получает список всех вакансий
+        """
         self.cursor.execute("""
             SELECT c.name, v.name, v.salary_from, v.salary_to, v.url
             FROM vacancies v
@@ -63,11 +86,11 @@ class DBManager:
         """)
         return self.cursor.fetchall()
 
-    def get_avg_salary(self):
+    def get_avg_salary(self) -> float:
         self.cursor.execute("""
             SELECT AVG(
-                CASE 
-                    WHEN salary_from IS NOT NULL AND salary_to IS NOT NULL 
+                CASE
+                    WHEN salary_from IS NOT NULL AND salary_to IS NOT NULL
                         THEN (salary_from + salary_to) / 2
                     WHEN salary_from IS NOT NULL THEN salary_from
                     WHEN salary_to IS NOT NULL THEN salary_to
@@ -75,28 +98,43 @@ class DBManager:
             )
             FROM vacancies
         """)
-        return round(self.cursor.fetchone()[0] or 0, 2)
+        result = self.cursor.fetchone()
+        if result is None or result[0] is None:
+            return 0.0
+        return round(result[0], 2)
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self) -> List[Tuple[str, str, Optional[int], Optional[int], str]]:
+        """
+        Получает список вакансий с зарплатой выше средней
+        """
         avg = self.get_avg_salary()
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
             SELECT c.name, v.name, v.salary_from, v.salary_to, v.url
             FROM vacancies v
             JOIN companies c ON v.company_id = c.id
             WHERE COALESCE(v.salary_from, v.salary_to, 0) > %s
-        """, (avg,))
+            """,
+            (avg,),
+        )
         return self.cursor.fetchall()
 
-    def get_vacancies_with_keyword(self, keyword):
-        self.cursor.execute("""
+    def get_vacancies_with_keyword(self, keyword: str) -> List[Tuple[str, str, Optional[int], Optional[int], str]]:
+        """
+        Получает список вакансий по ключевому слову в названии
+        """
+        self.cursor.execute(
+            """
             SELECT c.name, v.name, v.salary_from, v.salary_to, v.url
             FROM vacancies v
             JOIN companies c ON v.company_id = c.id
             WHERE v.name ILIKE %s
-        """, (f'%{keyword}%',))
+            """,
+            (f"%{keyword}%",),
+        )
         return self.cursor.fetchall()
 
-    def close(self):
+    def close(self) -> None:
+        """Закрытие соединения с БД"""
         self.cursor.close()
         self.conn.close()
-
